371b0718deb66c911682b382df5d2e23c4d39aa9
kamini <kaminikumari19@outlook.com>
2023-08-15 15:01:25 +0530

    made changes to Login.mjs

--- root/backend/RequestHandlers/Login.mjs
+++ root/backend/RequestHandlers/Login.mjs
@@ -10,4 +10,45 @@
 //  else
 //      send reponse {success:false, error:"Invalid username or password!"}
 
-export default function Login(req, res) {}
+// export default function Login(req, res) {}
+
+import User from "../Models/User.mjs";
+import bcrypt from 'bcrypt';
+import {jwt} from "jsonwebtoken";
+
+export async function login(req,res){
+    const{ username,password}=req.body;
+
+    try{
+        User.findOne({username})
+        .then(user =>{
+            bcrypt.compare(password,user.password)
+            .then(passwordCheck =>{
+                if(!passwordCheck) return res.status(400).send({error:"Don't have password"});
+
+                //create jwt token
+                const token= jwt.sign({
+                    userId: user._id,
+                    username : user.username
+                
+                }, ENV.JWT_SECRET, {expiresIN:"24h"});
+
+                return res.status(200).send({
+                    msg:"Login Successful..!",
+                    username:user.username,
+                    token
+                });
+
+            })
+            .catch(error =>{
+                return res.status(400).send({error:"password does not Match"})
+            })
+            
+        })
+        .catch(error =>{
+            return res.status(404).send({error :"Usename not found"});
+        })
+    }catch (error) {
+        return res.status(500).send({error});
+    }
+}
